1b4c151546eff62c76b42c70187189177c4b157b
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-13 23:43:58 +0530

    Update nc1.java_0.2

--- nc1.java
+++ nc1.java
@@ -3,12 +3,15 @@ public class nc1 {
         int sum = 0;
         System.out.println("Hello Narayana");
         int[] array1 = {12,13,14,15,16};
-        System.out.println(java.util.Arrays.toString(array1));
         for (int i = 0; i < array1.length; i++) {
-            sum++;
+            System.out.println(array1);    
         }
-        System.out.println("Total elements in array:" +sum);
-        //OR this is how i can measure the length of an array
-        System.out.println("Length of an array:" +array1.length);
+        System.out.println(java.util.Arrays.toString(array1));
+        // for (int i = 0; i < array1.length; i++) {
+        //     sum++;
+        // }
+        // System.out.println("Total elements in array:" +sum);
+        // //OR this is how i can measure the length of an array
+        // System.out.println("Length of an array:" +array1.length);
     }
 }
