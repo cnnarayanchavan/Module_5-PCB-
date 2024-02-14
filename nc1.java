b31f11f28cb3a6777f9073fb9ade88479dbef005
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-14 19:29:14 +0530

    Modified 0.3

--- nc1.java
+++ nc1.java
@@ -8,9 +8,9 @@ public class nc1 {
             System.out.println(array1);    
         }
         System.out.println(java.util.Arrays.toString(array1));
-        // for (int i = 0; i < array1.length; i++) {
-        //     sum++;
-        // }
+         for (int i = 0; i < array1.length; i++) {
+             sum++;
+        }
         // System.out.println("Total elements in array:" +sum);
         // //OR this is how i can measure the length of an array
         // System.out.println("Length of an array:" +array1.length);
