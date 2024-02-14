790e74f9a20277fee2330689e50a7f17f9cc6dfd
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-14 19:30:52 +0530

    modified nc0.4

---
+++ nc2.java
@@ -0,0 +1,20 @@
+public class nc2{
+    static void sortingArray(int[] Array1){
+        //System.out.println(java.util.Arrays.toString(arr));
+        for (int i = 0; i < Array1.length; i++) {
+            for (int j = 0; j < Array1.length-1-i; j++) {
+                if (Array1[j]>Array1[j+1]) {
+                    int temp = Array1[i];
+                    Array1[i] = Array1[j+1];
+                    Array1[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(java.util.Arrays.toString(Array1));
+
+    }
+    public static void main(String[] args) {
+        int[] arr ={4,6,7,8,3,9};
+        sortingArray(arr);
+    }
+}
