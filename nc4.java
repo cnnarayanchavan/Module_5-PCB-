f5fc965f6bc3aa74527f27efc0e620b4ab8052c3
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-15 23:51:37 +0530

    Update nc4.java_nc 0.8 Selection sort take_3

--- nc4.java
+++ nc4.java
@@ -1,6 +1,6 @@
 public class nc4{
   public static void main(String[] args){
-    int array = {34,56,67,23,41,14};
+    int[] array = {34,56,67,23,41,14};
     System.out.println("Before Sorting");
     System.out.println(java.util.Arraya.toString(array));
     System.out.println("After Sorting");
@@ -10,11 +10,11 @@ public class nc4{
   }
   public static void sortUsingSelection(int[] arr){
     for(int i=0; i<arr.length; i++){
-      let maxElement=i;
+      int maxElement=i;
       int temp =0;
       for(int j=i+1; i<arr.length; j++){
        if(arr[maxElement]>arr[j]){
-         maxElement=arr[j]
+         maxElement=arr[j];
        } 
         temp = arr[maxElement];
         arr[maxElement]=arr[j];
