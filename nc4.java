7165a87f7d1f018067f151b9365c3562456262d3
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-16 00:55:19 +0530

    Selection sort done finally at teke_12

--- nc4.java
+++ nc4.java
@@ -2,19 +2,22 @@ public class nc4{
   public static void main(String[] args) {
     int[] arr1={12,43,23,56,34};
     int temp =0;
-    for (int i = 0; i < arr1.length; i++) {
-      int maxElement =i;
-      for (int j = i+1; j < arr1.length; j++) {
-        if (arr1[maxElement]>arr1[j]) {
-          maxElement = arr1[j];
+    int maxElement =0;
+    for (int i = 0; i < arr1.length; i++) 
+    {
+       maxElement = i;
+      for (int j = i+1; j < arr1.length; j++) 
+      {
+        if (arr1[j]<arr1[maxElement]) {
+          maxElement = j;
         }
-        temp = arr1[i];
+      }
+      temp = arr1[i];
         arr1[i]=arr1[maxElement];
         arr1[maxElement]=temp;
-      }
-      for(i=0;i<arr1.length;i++){
-        System.out.print(arr1[i] + " ");
-      }
+    }
+    for(int i=0;i<arr1.length;i++){
+      System.out.print(arr1[i] + " ");
     }
   }
 }
