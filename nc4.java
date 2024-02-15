8c4bfd909a43148816e9de95f3a9aee7c2d8aaf7
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-16 00:25:43 +0530

    Update nc4.java_Selection sort nc_0.6_take_10

--- nc4.java
+++ nc4.java
@@ -23,7 +23,7 @@ public class nc4{
         arr[maxElement]=temp;
       }
        for(i=0; i<arr.length; i++){
-      System.out.println(array[i] + " ")
+      System.out.println(arr[i] + " ");
     }
     }
   }
