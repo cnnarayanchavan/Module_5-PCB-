c6f8728cbd598aea86306efda11e800c8b55552c
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-16 20:49:25 +0530

    Create nc5.java_Insertion sort eake_1

---
+++ nc5.java
@@ -0,0 +1,22 @@
+public class nc5{
+  public static void mina(String[]args){
+    int[] arr = {5,4,1,2,9};
+    sortUsingInsertion(arr);
+  }
+  public static sortUsingInsertion(int[] arr){
+    //here we will start the i from index 1 coz we assume the first element of an array is alrady sorted
+    for(int i = 1; i<arr.lngth; i++){
+      int temp = arr[i];
+      int j = i-1;
+      while(j>=0 && temp<arr[j]){
+        arr[j+1] = arr[j];
+        j--;
+      }
+    arr[j+1]=temp;
+    }
+    for(int i=0; i<arr.length; i++){
+      System.out.print(arr[i] + " ")
+    }
+    System.out.println();
+  }
+}
