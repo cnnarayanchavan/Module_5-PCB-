4349dc514f419da7168742f805223a1aba3014a5
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-16 20:52:41 +0530

    Update nc5.java_Insertion sort eake_2

--- nc5.java
+++ nc5.java
@@ -3,9 +3,9 @@ public class nc5{
     int[] arr = {5,4,1,2,9};
     sortUsingInsertion(arr);
   }
-  public static sortUsingInsertion(int[] arr){
+  public static void sortUsingInsertion(int[] arr){
     //here we will start the i from index 1 coz we assume the first element of an array is alrady sorted
-    for(int i = 1; i<arr.lngth; i++){
+    for(int i = 1; i<arr.length; i++){
       int temp = arr[i];
       int j = i-1;
       while(j>=0 && temp<arr[j]){
@@ -15,7 +15,7 @@ public class nc5{
     arr[j+1]=temp;
     }
     for(int i=0; i<arr.length; i++){
-      System.out.print(arr[i] + " ")
+      System.out.print(arr[i] + " ");
     }
     System.out.println();
   }
