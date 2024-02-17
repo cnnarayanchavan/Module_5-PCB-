481e29fa8956e6b482f599fc1104262f10862a2e
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-17 10:43:54 +0530

    revised the insertion sort take_1

---
+++ nc5.java
@@ -0,0 +1,16 @@
+public class nc5 {
+
+  public static void main(String[] args) {
+    int[]arr ={2,4,3,6,7};
+    for(int i=1; i<arr.length; i++){
+      int temp= arr[i];
+      int j=i-1;
+      while (j>=0 && temp<arr[j]) {
+        arr[j+1]=arr[j];
+        j--;
+      }
+      arr[j+1]=temp;
+    }
+    System.out.println(java.util.Arrays.toString(arr));
+  }
+}
