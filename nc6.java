5231418b7d2a36b5dab5542df3d8dc1c190f75bd
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-20 22:53:27 +0530

    Create nc6.java_ understood the functions in java_0001

---
+++ nc6.java
@@ -0,0 +1,12 @@
+//Functions in js
+public class nc6{
+  public static void main(String[] args){
+    //function for simple adding two numbers
+    int ans = printSum(12,12);
+      System.out.println(ans);
+    
+  }
+  static int printSum(int num1, int num2){
+    return num1 +num2;
+  }
+}
