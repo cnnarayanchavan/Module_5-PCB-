b6b80d9b1bc2b874bcfd02eb1f7f741212031c0f
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-20 23:39:00 +0530

    Resolved

--- nc6.java
+++ nc6.java
@@ -4,25 +4,26 @@ import java.util.Scanner;
 
 public class nc6{
   public static void main(String[]args){
-    Scanner sc = new Scanner(System.in);
-    int number = sc. nextInt();
-    System.out.println(printArm(number));
+    Scanner in = new Scanner(System.in);
+    int number = in. nextInt();
+    int ans = printArm(number);
+    System.out.println(ans);
   }
 
-  static int printArm(int Num){
-    int original = Num;
+  static int printArm(int n){
+    int original = n;
     int sum = 0;
-    while(Num<0){
-    int ld = Num%10;
-    Num = Num/10;
+    while(n>0){
+    int ld = n%10;
+    n = n/10;
     sum = sum + (ld*ld*ld);  
     }  
-    if(Num==original){
-      System.out.println("true");
-    }
-    else{
-      System.out.println("false");
-    }
+    // if(Num==original){
+    //   System.out.println("true");
+    // }
+    // else{
+    //   System.out.println("false");
+    // }
     return sum ;
   }
 }
