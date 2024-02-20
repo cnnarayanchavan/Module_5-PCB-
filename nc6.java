7588fc1b4559efeac3468acc1e2605685ec4d5a1
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-20 23:04:01 +0530

    Update nc6.java_understood functions in java_0003

--- nc6.java
+++ nc6.java
@@ -1,14 +1,34 @@
 //Functions in js
+import java.util.*;
 public class nc6{
   public static void main(String[] args){
     //function for simple adding two numbers
+    Scanner sc = new Scanner(System.in);
+    int sum = 0;
+    int num = sc.nextnt();
+    int original = num;
     int ans = printSum(12,12);
       System.out.println(ans);
     
   }
-  static int printSum(int num1, int num2){
-    return num1 +num2;
-  }
-}
-// this one is just the normal example of how function works in java
+  // static int printSum(int num1, int num2){
+  //   return num1 +num2;
+  // }
+  // this one is just the normal example of how function works in java.
 // que of printing armstrong numbers
+  static int printArmstrong(Number){
+    while(Number < 0){
+      LD = Number % 10;
+      Number = Number / 10;
+      sum = sum + (LD * LD * LD);
+    }
+    if(sum == Number){
+      System.out.println("True")
+    }
+    else "false"
+  }
+  return sum;
+}
+
+
+
