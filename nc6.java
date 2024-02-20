454e9ecfbb120554e6b8ba54198b4c5e0744f611
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-02-20 23:21:54 +0530

    s

--- nc6.java
+++ nc6.java
@@ -1,22 +1,17 @@
 //Functions in js
 import java.util.*;
-public class nc6{
-  public static void main(String[] args){
+public class nc6 {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
-    int ans = printArmstrong(num);
-    System.out.println(ans);
   }
-  static int printArmstrong(Number){
-    int sum = 0;
-    while(Number < 0){
-      LD = Number % 10;
-      Number = Number / 10;
-      sum = sum + (LD * LD * LD);  
+  static int printArm(Number){
+    while (Number<0) {
+      ld = Number%10;
+      Number = Number/10;
+      
     }
-    return sum;
   }
-
 }
 
 
