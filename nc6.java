44c2a11d2246a8519578ce9a95ea42ab511ec821
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-20 23:29:22 +0530

    Update nc6.java_done the implementation

--- nc6.java
+++ nc6.java
@@ -6,11 +6,23 @@ public class nc6{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     int number = sc. nextInt();
+    int original = number;
     System.out.println(printArm(number));
   }
 
   static int printArm(int Num){
     int sum = 0;
+    while(Num<0){
+    ld = Num%10;
+    Num = Num/10;
+    sum = sum + (ld*ld*ld);  
+    }  
+    if(Num==Original){
+      System.out.prinln("true")
+    }
+    else{
+      System.out.prinln("false")
+    }
     return sum ;
   }
 }
