dfccd835b278c8a77334eb32eff10d07799ebb12
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-21 22:46:46 +0530

    Update nc7.java_GNS_practice

--- nc7.java
+++ nc7.java
@@ -1,8 +1,8 @@
 public class nc7{
   public static void main(String[]args){
     int number = 4;
-    for(int i=1; i<=4; i++){
-      for(int j=1; j<=4; j++){
+    for(int i=1; i<=number; i++){
+      for(int j=1; j<=number; j++){
         System.out.print("*");
       }
         System.out.println();
