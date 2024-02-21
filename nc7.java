15388bed02b6b4605727398d544da77ccf44dd2e
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-21 22:45:33 +0530

    Create nc7.java_GNS Practice

---
+++ nc7.java
@@ -0,0 +1,11 @@
+public class nc7{
+  public static void main(String[]args){
+    int number = 4;
+    for(int i=1; i<=4; i++){
+      for(int j=1; j<=4; j++){
+        System.out.print("*");
+      }
+        System.out.println();
+    }
+  }
+}
