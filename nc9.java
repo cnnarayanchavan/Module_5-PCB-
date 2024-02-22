f2c4e0642cc8f8a9c89571ee750da710d4c4e21c
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-22 23:46:27 +0530

    Update nc9.java_revised bubble sort with time complexity n2(0)

--- nc9.java
+++ nc9.java
@@ -7,7 +7,7 @@ public class nc9{
         if(arr[j]>arr[j+1]){
           int temp = arr[j];
           arr[j]=arr[j+1];
-          arr[j+1]=arr[j];
+          arr[j+1]=temp;
         }
       }
     }
