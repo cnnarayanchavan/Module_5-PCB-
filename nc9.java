ad33e9169c016d55b1420b4bb1ee51b5a55d71e1
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-22 23:49:00 +0530

    Update nc9.java_revised bubble sort with time complexity n2(0)

--- nc9.java
+++ nc9.java
@@ -1,7 +1,8 @@
 // recise the sorting technique that we learnt 
 public class nc9{
   public static void main(String[] args){
-    int[] arr = {5,4,3,2,1};
+    //int[]arr = {5,4,3,2,1};
+    int[]arr = {56,35,76,45,89,67,45,87};
     for(int i=0; i<arr.length; i++){
       for(int j=0; j<arr.length-1; j++){
         if(arr[j]>arr[j+1]){
