94d4a33556cb910e786fa07ffbfae04b70f544a0
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-23 22:32:42 +0530

    Update nc10.java

--- nc10.java
+++ nc10.java
@@ -4,7 +4,7 @@ public class nc10{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);  
     int Num = sc.nextInt();
-    for(i=0; i<=Num; i++){
+    for(int i=0; i<=Num; i++){
       Num[i] = sc.nextInt();
     }
     System.out.println(java.util.Arrays.toString(Num));
