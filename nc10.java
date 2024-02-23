6658a71b57c16467e4ea706b01dd658f3a1a2913
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-23 22:14:38 +0530

    Update nc10.java_just printed the arrays_take01

--- nc10.java
+++ nc10.java
@@ -2,7 +2,7 @@
 import java.util.*;  //imported all needed packeges 
 public class nc10{
   public static void main(String[]args){
-    Scanner sc = new.Scanner(System.in);  
+    Scanner sc = new Scanner(System.in);  
     int Num = sc.nextInt();
     for(i=0; i<=Num; i++){
       Num[i] = sc.nextInt();
