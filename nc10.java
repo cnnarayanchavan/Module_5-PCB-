31b9d498deb5a6de46147f7b7e341a423bed5c8b
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-23 22:12:35 +0530

    Create nc10.java_just printed the array

---
+++ nc10.java
@@ -0,0 +1,12 @@
+// selection sort 
+import java.util.*;  //imported all needed packeges 
+public class nc10{
+  public static void main(String[]args){
+    Scanner sc = new.Scanner(System.in);  
+    int Num = sc.nextInt();
+    for(i=0; i<=Num; i++){
+      Num[i] = sc.nextInt();
+    }
+    System.out.println(Arrays.toString(Num));
+  }
+}
