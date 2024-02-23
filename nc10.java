fc3a6f6766415b37cbf9ff2c4d4cd93d5d95f57a
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-23 22:35:29 +0530

    Update nc10.java_ printed the array from user

--- nc10.java
+++ nc10.java
@@ -3,7 +3,13 @@ import java.util.*;  //imported all needed packeges
 public class nc10{
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);  
+    //for printing the arrays form user there are three type inwhich 
+    //1] we can directly diclear the array 
+    //2] we can initialise array first 
+    //3] aslo initialise the array first 
+    //as we are printing here so we have to only initialise it fisrt, then add the elements into it 
     int Num = sc.nextInt();
+    int arr[] = new int[Num]
     for(int i=0; i<=Num; i++){
       Num[i] = sc.nextInt();
     }
