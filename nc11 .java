1a25dc322753878b8e8e8ffa086553194ffebf48
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-24 23:52:45 +0530

    Update nc11 .java_runtime_polymorphism

--- nc11 .java
+++ nc11 .java
@@ -1,1 +1,18 @@
+public class nc11{
+  public static void main(String[]args){
+    Father obj1 = new Son();
+    obj1.fathersPhone();
+    obj1.fathersPhone();
+  }
+}
+class Father{
+  public static void fathersPhone(){
+    System.out.println("I have blueberry6600");
+  }
+}
+class Son extends Father{
+  public static void fathersPhone(){
+    System.out.println("I have Nokia6600");
+  }
+}
 
