6009e723f40145ff3849d653b87cd27772793a3b
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-25 22:24:25 +0530

    Update nc12.java_method Overloading

--- nc12.java
+++ nc12.java
@@ -1,1 +1,20 @@
-
+public class nc12{
+  public Static void main(String[]args){
+    Op o1 = new Op;
+    Op.operationOn("Narayana....!!")
+  }
+}
+class Op{
+  public Static int operationsOn(int num1, int num2){
+    return num1 + num2;
+  }
+  public Static int operationsOn(double num1, int num2){
+    return num1 + num2;
+}
+  public Static int operationsOn(int num1, int num2, int num3){
+    return num1 + num2;
+}
+  public Static Sting operationsOn(String name){
+    return System.out.println("Hey" + name);
+}
+}
