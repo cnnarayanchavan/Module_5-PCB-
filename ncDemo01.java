eba0af57b8b19016a96eba910c5faae31511d9f5
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-25 22:44:15 +0530

    Update ncDemo01.java__rendom for revision_01

--- ncDemo01.java
+++ ncDemo01.java
@@ -1,7 +1,7 @@
 public class ncDemo01{
   public static void main(String[]args){
-    int num = 5;
-    for(int i =0; i<num;i++){
+    int num = 4;
+    for(int i =1; i<num;i++){
       for(int j=0; j<num;j++){
         System.out.print("$");
       }
