48e04de39f1b9e3d0c30be8492fcb0a06db1e04c
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-02-25 22:42:54 +0530

    Update ncDemo01.java_pattern

--- ncDemo01.java
+++ ncDemo01.java
@@ -1,1 +1,11 @@
-
+public class ncDemo01{
+  public static void main(String[]args){
+    int num = 5;
+    for(int i =0; i<num;i++){
+      for(int j=0; j<num;j++){
+        System.out.print("$");
+      }
+      System.out.println();
+    }
+  }
+}
